20807c2cfcc91fb214d827459ba78a4c163db2f8
Yerlan <isaev.yerlan@kase.kz>
2022-04-21 12:55:21 +0600

    make it better

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {AppComponent} from "./app.component";
+import {AnimComponent} from "./anim/anim.component";
 
 const routes: Routes = [
   {
-    path: '/', component: AppComponent
+    path: '', component: AnimComponent
   }
 ];
 
